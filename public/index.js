ce892cd5e5a39c5facbd381404e200dec20a9f99
Alexey Gladyshev <aglad@pm.me>
2019-11-11 17:41:48 +0300

    feat: Update DOM after api call

--- public/index.js
+++ public/index.js
@@ -1,10 +1,16 @@
+let wrapper = document.getElementsByClassName('wrapper');
+
 fetch('http://localhost:3000/get', {
     headers: {'Content-Type':'application/json'},
     mode: 'cors',
     method: 'GET',
 })
 .then(function(res) {
-    console.log(res.json());
+    // console.log(res.json());
+    return res.json();
     }, function(error) {
     console.log(error);
-});
+})
+.then(function(json) {
+    wrapper[0].innerHTML = `<h1>${json.title}</h1>`;
+})
