acb4b9a00065418c624a5929a68101432df2bc4c
Alexey Gladyshev <aglad@pm.me>
2021-12-09 15:32:41 +0300

    move from deprecated request to axios

--- parser.js
+++ parser.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const parseString = require('xml2js').parseStringPromise;
-const request = require('request-promise-native');
+const axios = require('axios');
 const mongo = require('./mongo');
 
 var getFeedAll = function (req, res, next) {
     mongo.findPodcasts()
-    .then(function(podcasts) {
-        return Promise.all(podcasts.map((podcast) => getFeed(podcast))) 
-    })   
-    .then(function(podcasts) {
-        res.podcasts = podcasts;
-        next();
-    })
+        .then(function (podcasts) {
+            return Promise.all(podcasts.map((podcast) => getFeed(podcast.url)))
+        })
+        .then(function (podcasts) {
+            res.podcasts = podcasts;
+            next();
+        })
 }
 
 // var updateFeedAll = function (req, res, next) {
@@ -32,31 +32,32 @@ var getFeedAll = function (req, res, next) {
 
 
 var getFeed = function (url) {
-    return request(url)
-    .then(parseString)
-    .then(function (feed) {
-        return {
-            url: url,
-            feed: feed.rss.channel[0]
-        }
-    })
-    .catch(function (error) {
-        console.log(error);
-    })   
+    return axios.get(url)
+        .then((res) => res.data)
+        .then(parseString)
+        .then(function (feed) {
+            return {
+                url: url,
+                feed: feed.rss.channel[0]
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
 }
 
 var addPodcast = function (req, res, next) {
     getFeed(req.headers.url)
-    .then(function(podcast) {
-        mongo.insertPodcast(podcast);
-        res.podcast = podcast;
-        next();
-    })
+        .then(function (podcast) {
+            mongo.insertPodcast(podcast);
+            res.podcast = podcast;
+            next();
+        })
 }
-    
+
 app.use('/', express.static('public'));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
     next();
@@ -70,4 +71,4 @@ app.post('/add', addPodcast, function (req, res, next) {
     res.send(res.podcast);
 });
 
-app.listen(port, () => console.log(`Listening`)); 
+app.listen(port, () => console.log(`Listening`));
